952e0f9d2b4271b7f20e2225983f428c7f63ce2c
naitokosuke <102337893+naitokosuke@users.noreply.github.com>
2026-05-18 01:15:48 +0000

    feat: update vize to v0.104.0

--- flake.nix
+++ flake.nix
@@ -33,13 +33,13 @@
         in
         rustPlatform.buildRustPackage rec {
           pname = "vize";
-          version = "0.90.0";
+          version = "0.104.0";
 
           src = pkgs.fetchFromGitHub {
             owner = "ubugeeei";
             repo = "vize";
             rev = "v${version}";
-            hash = "sha256-dr4+3bzPRJDV+TVJ0l771Pm57AV0BVXCA2q83iXO/s4=";
+            hash = "sha256-Eh0aJ5/2FzpTpQKNr4gX6qsRKKhO+JYLDfqSX6XHo+A=";
           };
 
           cargoLock = {
